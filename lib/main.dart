9fe80e2b519b850c06a67ee979ec40dcaea00f2e
User-niomic <user@niomic.co.id>
2021-11-28 14:48:11 +0700

    Commit Tugas
    Tugas 1 Menggunakan Widget Container, Text, Center

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("SELAMAT BELAJAR"),
+        ),
+      ),
+    );
+  }
+}
